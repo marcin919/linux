faed050f2b6f6e86d1531798810ff052269084b4
marcin919 <mar.szmyd@gmail.com>
2020-02-12 13:14:09 +0100

    Update install_libreoffice-core.sh

--- install_libreoffice-core.sh
+++ install_libreoffice-core.sh
@@ -2,13 +2,11 @@
 # This will install all the requirements for libreoffice including the java-jre
 # For Ubuntu
 
-   
-   
-   sudo apt-get check
-   sudo apt-get update
-   sudo apt-get install -y libreoffice-core --no-install-recommends
-   sudo apt-get install -y libreoffice-java-common
-   sudo apt-get install -y openjdk-11-jre-headless
-   sudo apt-get install -y libreoffice-writer
+   sudo apt-get check > /home/last.txt
+   sudo apt-get update >> /home/last.txt
+   sudo apt-get install -y libreoffice-core --no-install-recommends >> /home/last.txt
+   sudo apt-get install -y libreoffice-java-common >> /home/last.txt
+   sudo apt-get install -y openjdk-11-jre-headless >> /home/last.txt
+   sudo apt-get install -y libreoffice-writer >> /home/last.txt
 
-   java -version
+   java -version >> /home/last.txt
