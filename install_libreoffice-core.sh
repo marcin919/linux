4376f385856d1eae958384b02c4c23c2e6ab8c1c
marcin919 <mar.szmyd@gmail.com>
2020-02-12 13:09:01 +0100

    Create install_libreoffice-core.sh

---
+++ install_libreoffice-core.sh
@@ -0,0 +1,11 @@
+   
+   
+   
+   sudo apt-get check
+   sudo apt-get update
+   sudo apt-get install -y libreoffice-core --no-install-recommends
+   sudo apt-get install -y libreoffice-java-common
+   sudo apt-get install -y openjdk-11-jre-headless
+   sudo apt-get install -y libreoffice-writer
+
+   java -version
